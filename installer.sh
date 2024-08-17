3eeaf0604797406eca9592576d7d12fe6d6a30ad
Kunnawuth Meebang <folknawat64@gmail.com>
2024-08-18 04:47:36 +0700

    update code

--- installer.sh
+++ installer.sh
@@ -6,26 +6,18 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install python3 python3-pip -y
 
 sudo apt install python3.10
-# cd Python-3.10.13
-# dir=$(dirname "$0") 
 
 # Create a virtual environment
 python3.10 -m venv /usr/local/python_test
 
 # Activate the virtual environment
 source /usr/local/python_test/bin/activate
-pip install -r /shinstall/requirements/requirements.txt
+/usr/local/python_dc/bin/python3 -m pip install -r /shinstall/requirements/requirements.txt
 
-# # Check if the necessary packages are installed
-# if ! pip freeze | grep -q 'Error'; then
-#     echo "Error: Failed to install"
-#     exit 1
-# fi
 
 # Copy .service files to systemd directory
 sudo rsync -r -az --delete -vt -v /shinstall/config/backend.service /etc/systemd/system/
 sudo rsync -r -az --delete -vt -v /shinstall/config/data.service /etc/systemd/system/
-
 
 # Reload systemd daemon and enable services
 sudo systemctl daemon-reload
