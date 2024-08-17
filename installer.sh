1122254926f533ac368fa3210cf3b818f3911bb8
Kunnawuth Meebang <folknawat64@gmail.com>
2024-08-18 03:22:36 +0700

    Demonstrate

--- installer.sh
+++ installer.sh
@@ -10,7 +10,6 @@ sudo apt update
 sudo apt upgrade -y
 # sudo apt install python3 python3-pip -y
 
-
 sudo apt install python3.10
 cd Python-3.10.13
 dir=$(dirname "$0") 
@@ -21,7 +20,7 @@ python3.10 -m venv /usr/local/python_test
 
 # Activate the virtual environment
 source /usr/local/python_test/bin/activate
-pip install -r $dir/requirements/requirements.txt
+pip install -r $dir/shinstall/requirements/requirements.txt
 
 # Check if the necessary packages are installed
 if ! pip freeze | grep -q 'Error'; then
@@ -29,14 +28,18 @@ if ! pip freeze | grep -q 'Error'; then
     exit 1
 fi
 
-# # Copy .service files to systemd directory
-# sudo rsync /Testttt/installer/config/backend.service /etc/systemd/system/
-# sudo rsync /Testttt/installer/config/data.service /etc/systemd/system/
+# Copy .service files to systemd directory
+sudo rsync -r -az --delete -vt -v $dir/shinstall/config/backend.service /etc/systemd/system/
+sudo rsync -r -az --delete -vt -v $dir/shinstall/config/data.service /etc/systemd/system/
 
-# # Reload systemd daemon and enable services
-# sudo systemctl daemon-reload
-# sudo systemctl enable backend.service
-# sudo systemctl enable data.service
+
+# Reload systemd daemon and enable services
+sudo systemctl daemon-reload
+sudo systemctl enable backend.service
+sudo systemctl enable data.service
+sudo systemctl restart data.service
+
+
 
 # # Start service
 # sudo systemctl start backend.service
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 
-#port 8080
+#def port 8080
+
 def root():
     return {"message": "Hello World"}
