1532f1131db658b6dff1713fd1bfbe35b29a4ebd
Kunnawuth Meebang <folknawat64@gmail.com>
2024-08-18 22:00:06 +0700

    fix code

--- installer.sh
+++ installer.sh
@@ -5,7 +5,7 @@ apt update -y
 apt install python3 python3-pip python3-venv -y
 
 python3 -m venv /usr/local/python_test
-# source /home/ubuntu2/shinstall/python_test/bin/activate
+source /home/ubuntu2/shinstall/python_test/bin/activate
 python3 -m pip install -r /home/ubuntu2/shinstall/requirements/requirements.txt
 
 # Copy .service files to systemd directory
