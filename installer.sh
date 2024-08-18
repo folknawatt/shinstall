274331f5144130e3b302503aaf5f9cfc9a8fc3a6
Kunnawuth Meebang <folknawat64@gmail.com>
2024-08-18 21:53:20 +0700

    update code

--- installer.sh
+++ installer.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-# Set default values for variables
-dir=$(dirname "$0")
-
 # Update and install packages
-# sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update && sudo apt upgrade -y
-sudo apt install python3 python3-pip -y
-sudo apt install python3-venv
+apt update -y
+apt install python3 python3-pip python3-venv -y
 
-python3 -m venv /home/ubuntu2/shinstall/python_test
-source /home/ubuntu2/shinstall/python_test/bin/activate
-python_test/bin/python3 -m pip install -r /home/ubuntu2/shinstall/requirements/requirements.txt
+python3 -m venv /usr/local/python_test
+python3 -m pip install -r /home/ubuntu2/shinstall/requirements/requirements.txt
 
 # Copy .service files to systemd directory
-sudo rsync -r -az --delete -vt -v /home/ubuntu2/shinstall/config/backend.service /etc/systemd/system/
-sudo rsync -r -az --delete -vt -v /home/ubuntu2/shinstall/config/data.service /etc/systemd/system/
+rsync -r -az --delete -vt -v /home/ubuntu2/shinstall/config/backend.service /etc/systemd/system/
 
-
-sudo systemctl daemon-reload
-sudo systemctl enable backend.service
-sudo systemctl enable data.service
-sudo systemctl restart data.service
-sudo systemctl start backend.service
-sudo systemctl status backend.service
-sudo systemctl status data.service
+systemctl enable backend.service
+systemctl restart backend.service
